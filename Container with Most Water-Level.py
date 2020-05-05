4d088e0c252b03f5a6157db2e9130edc1e2bd4d6
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-05 23:04:35 +0530

    Update Container with Most Water-Level.py

--- Container with Most Water-Level.py
+++ Container with Most Water-Level.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        area=[]
+        area=[]                                                     #creating a new list
         l=len(height)
-        for i in range(0,l):
+        for i in range(0,l):                                        #running the two loops from 0 to l and next from i+1 to l
             for j in range(i+1,l):
-                    ar=min(height[i],height[j])*(j-i)
-                    area.append(ar)
-        return max(area)           
-        
+                    ar=min(height[i],height[j])*(j-i)               #to calculate the every possible recatngle
+                    area.append(ar)                                 #storing it in the list
+        return max(area)                                            #returnig the max area 
