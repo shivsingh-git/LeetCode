f3676930474af288d897fa3856dd91adf2bda04b
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-06 20:27:05 +0530

    Update Jewels and Stone.py

--- Jewels and Stone.py
+++ Jewels and Stone.py
@@ -1,12 +1,12 @@
-class Solution:
-        def numJewelsInStones(self, J: str, S: str) -> int:
-            x , y =[] , []
+class Solution:                                                         
+        def numJewelsInStones(self, J: str, S: str) -> int:                  
+            x , y =[] , []                                                      #creating two lists
             for i in J :
-                x.append( i )
+                x.append( i )                                                   #adding the correspondins element in the lists
             for i in S :
                 y.append( i )
             c = 0
             for i in x :
-                c = c + y.count(i)
+                c = c + y.count(i)                                              #counting the values and storing in the counter
             return c
         
