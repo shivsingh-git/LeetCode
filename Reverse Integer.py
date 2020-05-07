dfc2df341d6972aafb5c302b4bdd5c9c183e63d7
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-07 23:12:10 +0530

    Update Reverse Integer.py

--- Reverse Integer.py
+++ Reverse Integer.py
@@ -1,13 +1,13 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x>= 2**31-1 or x<= -2**31: return 0
+        if x>= 2**31-1 or x<= -2**31: return 0                              #for checking the case to be in the given range of 2^32
         else:
             strg=str(x)
-            if(x>=0):
+            if(x>=0):                                                       #if the number is positive then just simply reverse the string
                 revst=strg[::-1]
             else:
-                temp=strg[1:]
+                temp=strg[1:]                                               #if negative then store the positive positive part only in the string
                 temp2=temp[::-1]
-                revst="-"+temp2
+                revst="-"+temp2                                             #reverse the strng and add negative to it
         if int(revst)>= 2**31-1 or int(revst)<=-2**31: return 0
-        else: return int(revst)
+        else: return int(revst)                                             #return the value if its in the range
