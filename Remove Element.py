c1468e2eba78ccb059e9dcafd02076f4e4c479dd
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-07 23:00:08 +0530

    Update Remove Element.py

--- Remove Element.py
+++ Remove Element.py
@@ -2,7 +2,7 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         count = 0
         for i in range(len(nums)):
-            if nums[i] != val :
-                nums[count] = nums[i]
-                count +=1
+            if nums[i] != val :             #checking if thr value is not equal to the list element
+                nums[count] = nums[i].      #alloat the value to the list
+                count +=1.                  #increase the value of count
         return count
