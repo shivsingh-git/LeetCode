859919b2b53d8d1a1748bc2108f48ad9b66f930f
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-20 22:52:30 +0530

    Update Maximum Subarray.py

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        temp = []                               
-        temp.append(nums[0])
+        temp = []                                                   #creating a list to store the maximum values              
+        temp.append(nums[0])                                        #storing the first value in the list
         for i in range(1,len(nums)):
-            r = max(nums[i],temp[i-1]+nums[i])
-            temp.append(r)
-        return max(temp)
+            r = max(nums[i],temp[i-1]+nums[i])                      #getting the sum value of evry subarrays
+            temp.append(r)                                          #storing the sums in the temp list
+        return max(temp)                                            #returning the maximum value
