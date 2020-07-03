9cb700354889b0dd4a94d4fecf05405f9097cb03
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-07-03 20:21:58 +0530

    Update Plus One.py

--- Plus One.py
+++ Plus One.py
@@ -1,7 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        s = ""
+        s = ""                                                      #declaring the string
         for i in digits:
-            s += str(i)
-        return [x for x in str(int(s) + 1)]
-        
+            s += str(i)                                             #adding the numbers in the string
+        return [x for x in str(int(s) + 1)]                         # returning the value adding 1 to last digit
