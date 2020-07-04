1358ecf7cd5289d76b4661ca35360a158bb97b58
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-07-04 22:45:35 +0530

    Update Remove Element.py

--- Remove Element.py
+++ Remove Element.py
@@ -3,6 +3,6 @@ class Solution:
         count = 0
         for i in range(len(nums)):
             if nums[i] != val :             #checking if thr value is not equal to the list element
-                nums[count] = nums[i].      #alloat the value to the list
+                nums[count] = nums[i].      #allot the value to the list
                 count +=1.                  #increase the value of count
         return count
