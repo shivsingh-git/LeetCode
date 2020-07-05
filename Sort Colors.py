ce1b9bc555a7d9172b584940a3a8739732ea1020
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-07-05 20:32:28 +0530

    Create Sort Colors.py

---
+++ Sort Colors.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        nums.sort()
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
